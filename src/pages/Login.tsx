23c37b1a1bdee3e88f4f1847676c32092470c3bf
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-03 17:45:26 +0800

    feat: Implement login page with email and password fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,69 @@
-import { InputField } from "../components"
+import { useState } from "react";
+import { Button, InputField } from "../components"
+import { useNavigate } from "react-router";
 
-export const Login = () => {
+export const Login: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const handleEmailChange = (value: string) => {
+    setEmailValue(value);
+  };
+
+  const handlePasswordChange = (value: string) => {
+    setPasswordValue(value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Input Value:", emailValue, passwordValue);
+  };
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
   return <>
-    <InputField
-      type="text"
-      placeholder="Email"
-    />
+    <h2 className="
+      text-custom-black
+      text-xl
+      mb-9
+    ">
+      到元宇宙展開全新社交圈
+    </h2>
+
+    <div className="mb-4">
+      <InputField
+        type="text"
+        placeholder="Email"
+        value={emailValue}
+        onChange={handleEmailChange}
+      />
+    </div>
+
+    <div className="mb-8">
+      <InputField
+        type="password"
+        placeholder="Password"
+        value={passwordValue}
+        onChange={handlePasswordChange}
+      />
+    </div>
+
+    <div className="mb-4">
+      <Button
+        text="登入"
+        onClick={handleSubmit}
+      />
+    </div>
+
+    <div className="flex justify-center">
+      <Button
+        text="註冊"
+        theme="secondary"
+        onClick={handleRegister}
+      />
+    </div>
   </>
 }

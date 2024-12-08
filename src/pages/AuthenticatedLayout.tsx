7bc1db9b269ee4f91e6acc16d288823acf458965
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-08 10:43:48 +0800

    refactor: Add layout container for `Outlet` in `AuthenticatedLayout` - Wrapped `Outlet` with a container div to apply `max-w-4xl`, `px-2`, and `mx-auto` classes.

--- src/pages/AuthenticatedLayout.tsx
+++ src/pages/AuthenticatedLayout.tsx
@@ -4,6 +4,13 @@ import { Outlet } from "react-router";
 export const AuthenticatedLayout: React.FC = () => {
   return <>
     <HeaderBar />
-    <Outlet />
+    <div className="
+      max-w-4xl
+      pt-12
+      px-2
+      mx-auto
+    ">
+      <Outlet />
+    </div>
   </>
 }

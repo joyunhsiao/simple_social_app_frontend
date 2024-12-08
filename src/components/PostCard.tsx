8af2ff23fec0171accbcc69d8015c507c245a3f3
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-08 20:25:20 +0800

    feat: Redesign `PostCard` with enhanced structure and functionality
    - Added `PostCardProps` interface to support `name` and `isImg` props for flexibility.
    - Incorporated `useNavigate` for navigation functionality on name click.
    - Integrated `CircularImageWrapper` for profile image and `SquareImageWrapper` for optional post images.
    - Styled the card with custom classes for layout, spacing, and hover effects.
    - Enabled conditional rendering of images based on the `isImg` prop.

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,72 @@
-export const PostCard = () => {
+import { useNavigate } from "react-router";
+import { CircularImageWrapper } from "./CircularImageWrapper";
+import { SquareImageWrapper } from "./SquareImageWrapper";
+
+interface PostCardProps {
+  name: string;
+  isImg?: boolean;
+}
+
+export const PostCard: React.FC<PostCardProps> = ({
+  name,
+  isImg = false
+}) => {
+  const navigate = useNavigate();
+
+  const handleNameClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    navigate("/"); // todo
+  };
+
   return <>
-    PostCard
+    <div className="
+      bg-custom-white
+      text-custom-black
+      rounded-lg
+      ring-2
+      ring-custom-black
+      p-6
+      mb-4
+    ">
+      <div className="
+        flex
+        items-center
+        gap-4
+        mb-4
+      ">
+        <CircularImageWrapper
+          imgSrc="../src/assets/profile_photo.jpg"
+        />
+        <div className="
+          w-full
+        ">
+          <a href="#" onClick={(e) => handleNameClick(e)} className="
+            text-custom-black
+            font-bold
+            hover:underline
+            hover:text-custom-blue
+          ">
+            {name}
+          </a>
+          <p className="
+            text-custom-gray
+            text-xs
+          ">
+            8/12/2024 12:00
+          </p>
+        </div>
+      </div>
+      <p>
+        Content
+      </p>
+      {isImg && (
+        <div className="mt-4">
+          <SquareImageWrapper
+            imgSrc="../src/assets/post_photo.jpg"
+            alt="post photo"
+          />
+        </div>
+      )}
+    </div>
   </>
 }

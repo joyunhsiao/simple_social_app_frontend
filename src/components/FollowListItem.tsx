114c584f0cefd7f9ff44ef3f6c00954b5ec9dae4
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-08 15:12:30 +0800

    feat: Add `FollowListItem` component
    - Added `FollowListItem` component to display individual follow list items.
    - The component includes a circular image, follow time, and follow duration.
    - Applied custom styling for layout and interactive elements.

--- src/components/FollowListItem.tsx
+++ src/components/FollowListItem.tsx
@@ -1,5 +1,63 @@
-export const FollowListItem = () => {
-  return <>
-    FollowListItem
-  </>
+import { useNavigate } from "react-router";
+import { CircularImageWrapper } from "./CircularImageWrapper"
+
+interface FollowListItemProps {
+  name: string;
 }
+
+export const FollowListItem: React.FC<FollowListItemProps> = ({
+  name
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    navigate("/"); // todo
+  };
+
+  return <>
+    <div className="
+      flex
+      items-center
+      gap-4
+      bg-custom-white
+      ring-2
+      ring-custom-black
+      rounded-lg
+      p-4
+      mb-4
+    ">
+      <CircularImageWrapper
+        imgSrc="../src/assets/profile_photo.jpg"
+      />
+      <div className="
+        w-full
+      ">
+        <a href="#" onClick={(e) => handleClick(e)} className="
+          text-custom-black
+          font-bold
+          hover:underline
+          hover:text-custom-blue
+        ">
+          {name}
+        </a>
+        <div className="
+          flex
+          justify-between
+          text-sm
+        ">
+          <p className="
+            text-custom-gray
+          ">
+            Follow Time: 8/12/2024 12:00
+          </p>
+          <p className="
+            text-custom-black
+          ">
+            You have followed for 90 days!
+          </p>
+        </div>
+      </div>
+    </div>
+  </>
+};

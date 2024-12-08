5aae936bb3de2e3ed6e02d8131d1fce2d6f8894d
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-08 19:31:56 +0800

    feat: Enhance `SearchBar` component with functionality and styling
    - Added `SearchBarProps` interface to define props: `value`, `onChange`, and `onClick`.
    - Implemented input handling with `onChange` for dynamic search term updates.
    - Added a search button with an `onClick` handler and an icon from `react-icons/fi`.
    - Applied custom styling for responsive layout and visual consistency.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,60 @@
-export const SearchBar = () => {
+import { FiSearch } from "react-icons/fi";
+
+interface SearchBarProps {
+  value: string;
+  onChange: (value: string) => void;
+  onClick: () => void;
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({
+  value,
+  onChange,
+  onClick
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const handleClick = () => {
+    onClick();
+  };
+
   return <>
-    SearchBar
+    <div className="
+      w-full
+      flex
+      ring-2
+      ring-custom-black
+    ">
+      <input
+        type="text"
+        placeholder="Search Posts"
+        value={value}
+        onChange={handleChange}
+        className="
+          w-full
+          bg-custom-white
+          border-r-2
+          border-custom-black
+          py-3
+          px-4
+          placeholder:text-custom-gray
+          focus:outline-none
+          focus:ring-inset
+          focus:ring-2
+      "/>
+      <button
+        type="button"
+        title="search"
+        onClick={handleClick}
+        className="
+          text-custom-white
+          bg-custom-blue
+          p-3
+          hover:bg-custom-yellow
+      ">
+        <FiSearch size={20} />
+      </button>
+    </div>
   </>
 }

9e92e05aa57f6a2d2fe829975a55d4870d867827
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-08 20:39:20 +0800

    feat: Enhance `Home` component with search and post display functionality
    - Added `SearchBar` component for user input with `onChange` and `onClick` handlers.
    - Implemented state management for search input using `useState`.
    - Included multiple `PostCard` components with support for optional images.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
-export const Home = () => {
+import { useState } from "react"
+import { PostCard, SearchBar } from "../components"
+
+export const Home: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchValueChange = (value: string) => {
+    setSearchValue(value);
+  };
+
+  const handleSubmit = () => {
+    console.log("Input Value:", searchValue);
+  };
+
   return <>
-    Home
+    <div className="
+      mb-4
+    ">
+      <SearchBar value={searchValue} onChange={handleSearchValueChange} onClick={handleSubmit}/>
+    </div>
+    <PostCard name="A" isImg={true} />
+    <PostCard name="B" />
+    <PostCard name="C" isImg={true} />
+    <PostCard name="D" />
   </>
 }

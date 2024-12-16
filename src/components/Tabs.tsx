ce62363006f6982909fb35be120467a8c355bb5d
Jo Yun Hsiao <joyunhsiao@gmail.com>
2024-12-16 21:08:06 +0800

    feat: Refactor Tabs component and add dynamic tab content
    - Introduced `TabsProps` interface to define `tabOne`, `tabTwo`, `childrenOne`, and `childrenTwo` props.
    - Added `useState` to manage active tab state.
    - Updated `Tabs` component to dynamically render content based on the active tab.
    - Improved tab styling with conditional classes for active and inactive tabs.

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,65 @@
-export const Tabs = () => {
+import { ReactNode, useState } from "react";
+
+interface TabsProps {
+  tabOne: string;
+  tabTwo: string;
+  childrenOne: ReactNode;
+  childrenTwo: ReactNode;
+}
+
+export const Tabs: React.FC<TabsProps> = ({
+  tabOne,
+  tabTwo,
+  childrenOne,
+  childrenTwo
+}) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabsTitleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, index:number) => {
+    e.preventDefault();
+    setActiveTab(index);
+  };
+
   return <>
-    Tabs
+    <div className="
+      pt-4
+    ">
+      <div className="
+        flex
+        px-4
+      ">
+        <a href="#" onClick={(e) => handleTabsTitleClick(e, 0)} className={`
+          block
+          border-2
+          border-custom-black
+          rounded-t-lg
+          px-6
+          py-2
+          ${activeTab === 1 ? "bg-custom-white" : "bg-custom-black"}
+          ${activeTab === 1 ? "text-custom-black" : "text-custom-white"}
+        `}>{tabOne}</a>
+        <a href="#" onClick={(e) => handleTabsTitleClick(e, 1)} className={`
+          block
+          border-2
+          border-custom-black
+          rounded-t-lg
+          px-6
+          py-2
+          ${activeTab === 1 ? "bg-custom-black" : "bg-custom-white"}
+          ${activeTab === 1 ? "text-custom-white" : "text-custom-black"}
+        `}>{tabTwo}</a>
+      </div>
+      <div className="
+        bg-custom-white
+        ring-2
+        ring-custom-black
+        rounded-lg
+        py-8
+        px-24
+        sm:px-28
+      ">
+        {activeTab === 1 ? childrenTwo : childrenOne}
+      </div>
+    </div>
   </>
 }
